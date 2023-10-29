d10f7331a6bc57921e551cc6517e86bfffc94f5b
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-10-29 08:32:44 +0530

    static props added in index.js

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,6 +1,6 @@
-import { GetServerSidePropsContext, PreviewData } from "next";
+import * as fs from "fs";
+import { GetStaticPropsContext } from "next";
 import Head from "next/head";
-import { ParsedUrlQuery } from "querystring";
 
 // const fetchOneBlog = async (query: string) => {
 //   const data = await fetch(`/api/blog?slug=${query}`);
@@ -13,17 +13,17 @@ interface IData {
   author: string;
   title: string;
 }
-export const getServerSideProps = async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
-  try {
-    if (context.params !== null && context.params !== undefined) {
-      const data = await fetch(`http://localhost:3000/api/blog?slug=${context.params.slug}`);
-      const res = await data.json();
-      return { props: { res } };
-    }
-  } catch (error) {
-    return { props: { res: {} } }; // Return empty array or some default value
-  }
-};
+// export const getServerSideProps = async (context: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
+//   try {
+//     if (context.params !== null && context.params !== undefined) {
+//       const data = await fetch(`http://localhost:3000/api/blog?slug=${context.params.slug}`);
+//       const res = await data.json();
+//       return { props: { res } };
+//     }
+//   } catch (error) {
+//     return { props: { res: {} } }; // Return empty array or some default value
+//   }
+// };
 
 export default function Page({ res }: { res: IData }) {
   return (
@@ -37,3 +37,38 @@ export default function Page({ res }: { res: IData }) {
     </>
   );
 }
+
+export async function getStaticPaths() {
+  try {
+    const data = await fetch("http://localhost:3000/api/blogs");
+    const res = await data.json();
+    const allPaths = await res.map((item: IData) => {
+      return {
+        params: {
+          slug: item.title.replace(/\s+/g, "-"),
+        },
+      };
+    });
+    // console.log(allPaths, "All paths");
+    return {
+      paths: allPaths,
+      fallback: false,
+    };
+  } catch (error) {
+    return { props: { res: [] } }; // Return empty array or some default value
+  }
+}
+// <ParsedUrlQuery, PreviewData>
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  try {
+    const { slug } = context.params || {};
+    if (!slug) {
+      throw new Error("Slug not found");
+    }
+
+    const data = fs.readFileSync(`blogData/${slug}.json`, "utf-8");
+    return { props: { data } };
+  } catch (error) {
+    return { props: { res: [] } };
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { GetServerSideProps } from "next";
+// import type { GetServerSideProps } from "next";
+import * as fs from "fs";
 import Home from "./Home";
 
 interface IData {
@@ -6,17 +7,55 @@ interface IData {
   author: string;
   title: string;
 }
-export const getServerSideProps = (async () => {
+/**
+ * it will fetch the data on server client do need to make the api call and the data will be passed as props
+ */
+// export const getServerSideProps = (async () => {
+//   try {
+//     const data = await fetch("http://localhost:3000//api/blogs");
+//     const res = await data.json();
+//     return { props: { res } };
+//   } catch (error) {
+//     return { props: { res: [] } }; // Return empty array or some default value
+//   }
+// }) satisfies GetServerSideProps<{
+//   res: IData[];
+// }>;
+
+/**
+ * get static props
+ */
+
+export async function getStaticProps() {
   try {
-    const data = await fetch("http://localhost:3000//api/blogs");
-    const res = await data.json();
+    const res: IData[] = [];
+    const blogsData = await new Promise<string[]>((resolve, reject) => {
+      fs.readdir("blogData", (err, data) => {
+        if (err) {
+          reject("An Error occurred");
+        } else {
+          resolve(data);
+        }
+      });
+    });
+    const promises = blogsData.map((item) => {
+      return new Promise<void>((resolve, reject) => {
+        fs.readFile(`blogData/${item}`, "utf8", function (err, data) {
+          if (err) {
+            reject({ error: "no blog found" });
+          } else {
+            res.push(JSON.parse(data));
+            resolve();
+          }
+        });
+      });
+    });
+    await Promise.all(promises);
     return { props: { res } };
   } catch (error) {
     return { props: { res: [] } }; // Return empty array or some default value
   }
-}) satisfies GetServerSideProps<{
-  res: IData[];
-}>;
+}
 
 export default function Main({ res }: { res: IData[] }) {
   return <Home res={res} />;
