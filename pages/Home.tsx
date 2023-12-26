ef4092ff63d7458e87cb6b23eb84342fba30b5a2
NidhiSharma63 <nidhisharma639593@gmail.com>
2023-12-26 08:22:25 +0530

    SEO tag added

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -8,18 +8,36 @@ interface IData {
   author: string;
   title: string;
 }
-const Home = ({ res }: { res: IData[] }) => {
+const Home = ({res}: {res: IData[]}) => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Your Blog Title | React, JavaScript, HTML, CSS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="In-depth tutorials and articles about React, JavaScript, HTML, CSS, and React Hooks. Learn the latest in web development and front-end frameworks."
+        />
+        <meta property="og:title" content="Your Blog Title" />
+        <meta
+          property="og:description"
+          content="Expert insights and tutorials on React, JavaScript, HTML, CSS, and React Hooks."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.yourwebsite.com" />
+        <meta charset="UTF-8" />
+        <meta name="author" content="Nidhi Sharma" />
+        <meta
+          name="keywords"
+          content="React, JavaScript, HTML, CSS, React Hooks, Web Development, Front-end Development"
+        />
       </Head>
       <div className="gap-5 flex justify-center flex-col items-center w-full">
         <h1 className="text-5xl">Hunter Code</h1>
         <Image className={styles.homeImg} src="/images/coderImg.avif" alt="coder image" width={200} height={200} />
         <div className="mt-10 flex justify-center flex-col items-center w-full">
           <h1 className="text-2xl">Latest Blogs</h1>
-          {res?.map((item: { title: string; content: string; author: string }, index: number): JSX.Element => {
+          {res?.map((item: {title: string; content: string; author: string}, index: number): JSX.Element => {
             return (
               <div key={index} className="mt-10 border-2 border-black rounded border-solid p-2">
                 <Link href={`/blog/${item.title}`}>
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,5 +1,5 @@
 import * as fs from "fs";
-import { GetStaticPropsContext } from "next";
+import {GetStaticPropsContext} from "next";
 import Head from "next/head";
 
 // const fetchOneBlog = async (query: string) => {
@@ -25,10 +25,30 @@ interface IData {
 //   }
 // };
 
-export default function Page({ res }: { res: IData }) {
+export default function Page({res}: {res: IData}) {
   return (
     <>
-      <Head>{/* <title>{router.query.slug}</title> */}</Head>
+      <Head>
+        <title>{res.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="In-depth tutorials and articles about React, JavaScript, HTML, CSS, and React Hooks. Learn the latest in web development and front-end frameworks."
+        />
+        <meta property="og:title" content="Your Blog Title" />
+        <meta
+          property="og:description"
+          content="Expert insights and tutorials on React, JavaScript, HTML, CSS, and React Hooks."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.yourwebsite.com" />
+        <meta charset="UTF-8" />
+        <meta name="author" content="Nidhi Sharma" />
+        <meta
+          name="keywords"
+          content="React, JavaScript, HTML, CSS, React Hooks, Web Development, Front-end Development"
+        />
+      </Head>
       <div className="gap-5 flex justify-center flex-col items-center w-full">
         <h1 className="text-lg font-medium">Author : {res.author}</h1>
         <h1 className="text-lg font-medium">Title of our page is : {res.title}</h1>
@@ -61,22 +81,21 @@ export async function getStaticPaths() {
       fallback: false,
     };
   } catch (error) {
-    return { props: { res: [] } }; // Return empty array or some default value
+    return {props: {res: []}}; // Return empty array or some default value
   }
 }
 // <ParsedUrlQuery, PreviewData>
 export const getStaticProps = async (context: GetStaticPropsContext) => {
-  console.log(context, "This is context");
   try {
-    const { slug } = context.params || {};
+    const {slug} = context.params || {};
     if (!slug) {
       throw new Error("Slug not found");
     }
     if (typeof slug === "string") {
       const res = fs.readFileSync(`blogData/${slug.replace(/ /g, "-")}.json`, "utf-8");
-      return { props: { res: JSON.parse(res) } };
+      return {props: {res: JSON.parse(res)}};
     }
   } catch (error) {
-    return { props: { res: [] } };
+    return {props: {res: []}};
   }
 };
